4404458548a0e2310be74273127f93c597fc72df
PranjalBuilds <imcae22060@gmail.com>
2025-01-09 16:12:05 +0530

    Operators

--- day1.py
+++ day1.py
@@ -5,3 +5,7 @@ print("Hello World");
 #Escape Sequence Characters
 print('Hey I am \"Pranjal Thakkar\" and I am learning Python');
 
+#Separator
+print('Hey','I','am','Pranjal','Thakkar',sep=' ~ ');
+
+#Operators
