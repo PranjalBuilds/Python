10b2fe3e78de987e82ba17fedef71ab5207be359
PranjalBuilds <imcae22060@gmail.com>
2025-01-09 16:12:59 +0530

    Arithmatic Operations

--- day1.py
+++ day1.py
@@ -9,3 +9,33 @@ print('Hey I am \"Pranjal Thakkar\" and I am learning Python');
 print('Hey','I','am','Pranjal','Thakkar',sep=' ~ ');
 
 #Operators
+#Arithmatic Operators
+# + - * / // % **
+
+#addition
+result = 5 + 5
+print(result)
+
+#subtraction
+result = 5 - 5
+print(result)
+
+#multiplication
+result = 5 * 5
+print(result)
+
+#division
+result = 5 / 5
+print(result)
+
+#floor division
+result = 17 // 4
+print(result)
+
+#modulus
+result = 17 % 4
+print(result)
+
+#exponentiation
+result = 5**5
+print(result)
