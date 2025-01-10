8c48c2b039523e763fa2a7a2e00c8e7420985866
PranjalBuilds <imcae22060@gmail.com>
2025-01-10 16:22:39 +0530

    range()

--- day2.py
+++ day2.py
@@ -10,3 +10,14 @@ else:
 
 #Ternary Operator
 print('a is larger ' if a > b else 'b is larger')
+
+
+#Range function
+for i in range(10):
+    print(i) # 0 to 9
+
+for i in range(1, 10):
+    print(i+1)  # 1 to 10
+
+for i in range(1, 10, 2):
+    print(i) #1 3 5 7 9
