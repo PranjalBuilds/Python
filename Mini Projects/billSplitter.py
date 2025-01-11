f9fefcaf88187b12e9630373bd0119dbf82eef87
PranjalBuilds <imcae22060@gmail.com>
2025-01-11 23:21:05 +0530

    Bill Splitter

---
+++ Mini Projects/billSplitter.py
@@ -0,0 +1,14 @@
+def add():
+    totalAmt = float(input('Enter total amount: ₹'))
+    totalPerson = float(input('Enter total person: '))
+    
+    if totalPerson == 0 or totalAmt == 0:
+        print("Error: The number of persons or total amount cannot be zero.")
+        return
+    if totalAmt < 0 or totalPerson < 0:
+        print("Error: Total amount and total persons must be positive numbers.")
+        return
+    
+    amountPerPerson = f"{(totalAmt / totalPerson):.2f}"
+    print('Amount per person is', amountPerPerson)
+add()
