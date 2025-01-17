6e000dec1b4cc169c31f57a53cd4c96b3188b53c
PranjalBuilds <imcae22060@gmail.com>
2025-01-17 14:53:51 +0530

    Set

---
+++ day9.py
@@ -0,0 +1,17 @@
+# Set
+
+# set is a collection of well-defined objects.
+# in sets, there are only unique members.
+
+setEx = {1,2,3,4,1,2}
+print(setEx) # 1 2 3 4
+
+a = {1,False,'John',19.4,-2}
+print(a)
+
+#empty set
+b = set()
+
+# Iterating data from set
+for i in a:
+    print(i)
