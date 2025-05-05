ab1eb8a2ab7ede225ecb7ec226136fb2494affe7
PranjalBuilds <imcae22060@gmail.com>
2025-05-05 23:36:51 +0530

    Time module

---
+++ day36.py
@@ -0,0 +1,30 @@
+import time 
+
+# def usingWhile():
+#     i = 0
+#     while i < 5000:
+#         i += 1
+#         print(i)
+    
+# def usingFor():
+#     for i in range(5000):
+#         print(i)
+
+# init = time.time()
+# usingFor()
+# t1 = (time.time() - init)
+
+# init = time.time()
+# usingWhile()
+# print(time.time() - init)
+# print(t1)
+
+# print(5)
+# time.sleep(3)
+# print("This is printed after 3 seconds!")
+
+
+# t = time.localtime()
+# formatted_time = time.strftime("%d-%m-%Y %H:%M:%S", t)
+
+# print(formatted_time)
