f77e955da089bb70cad04589c16277edce9058f6
PranjalBuilds <imcae22060@gmail.com>
2025-05-13 23:52:06 +0530

    Sh-Util

---
+++ day39.py
@@ -0,0 +1,12 @@
+import shutil as s
+import os 
+
+# s.copy("day39.py", "day39Copy.py")
+
+# s.copytree("temp", "tempCopy")
+
+# s.move('temp/temp.py', "tempCopy.py")
+
+# s.rmtree("temp")
+
+# os.remove("day39Copy.py")
---
+++ day39Copy.py
@@ -0,0 +1,5 @@
+import shutil as s
+
+s.copy("day39.py", "day39Copy.py")
+
+s.copytree("temp", "tempCopy")
