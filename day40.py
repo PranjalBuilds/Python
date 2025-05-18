ea7ce2b08733652c189bd655127b1bed8e35c238
PranjalBuilds <imcae22060@gmail.com>
2025-05-18 23:57:17 +0530

    Post requests

--- day40.py
+++ day40.py
@@ -1,6 +1,22 @@
 # Request module
 
 import requests as req
-res = req.get("https://www.google.com")
 
+#res = req.get("https://www.google.com")
+#print(res.text)
+
+url = "https://jsonplaceholder.typicode.com/posts"
+
+data = {
+    "title": "foo",
+    "body": "bar",
+    "userId": 1
+}
+
+headers = {
+    "Content-type": "application/json; charset=UTF-8"
+}
+
+
+res = req.post(url, headers=headers, json=data)
 print(res.text)
